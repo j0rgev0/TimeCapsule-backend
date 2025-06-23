9c249214d758dd383b340327f70b24fd233ea1f9
IvanNavas092 <navass21112@gmail.com>
2025-06-23 13:38:19 +0200

    change typed id

--- src/main/java/com/Timecapsule/timecapsule/repository/CapsuleRepository.java
+++ src/main/java/com/Timecapsule/timecapsule/repository/CapsuleRepository.java
@@ -1,9 +1,11 @@
 package com.Timecapsule.timecapsule.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Timecapsule.timecapsule.models.Capsule;
 
-public interface CapsuleRepository extends JpaRepository<Capsule, Long> {
+public interface CapsuleRepository extends JpaRepository<Capsule, UUID> {
   
 }
