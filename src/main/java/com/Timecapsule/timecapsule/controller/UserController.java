587df8e305ac9a3aa788d5687eaa2fadd31f71ff
Jorge <jorgevivar2005@gmail.com>
2025-06-27 19:49:40 +0200

    Add endpoint to retrieve current user information in UserController, utilizing Spring Security for authentication context management.

--- src/main/java/com/Timecapsule/timecapsule/controller/UserController.java
+++ src/main/java/com/Timecapsule/timecapsule/controller/UserController.java
@@ -2,7 +2,10 @@ package com.Timecapsule.timecapsule.controller;
 
 import java.util.List;
 
+import com.Timecapsule.timecapsule.dto.CapsulesDtos.CapsuleDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +28,15 @@ public class UserController {
   public ResponseEntity<List<UserDto>> getAllUsers(){
     List<UserDto> userDtos = userService.getAll();
     return ResponseEntity.ok(userDtos);
+  }
+
+  @GetMapping("/current")
+  public ResponseEntity<UserDto> getCurrentUser(){
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    UserDto currentUser = (UserDto) authentication.getPrincipal();
+
+
+    return ResponseEntity.ok(currentUser);
   }
 
 }
