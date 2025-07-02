690c60d08d7e4e4963ec4acf55b55164bbf21c35
Jorge <jorgevivar2005@gmail.com>
2025-07-02 20:05:27 +0200

    Add getAllImages endpoint in ImagesController, implement image retrieval logic in ImageService, and enhance ImageMapper for mapping Image to ImagesDto.

--- src/main/java/com/Timecapsule/timecapsule/controller/ImagesController.java
+++ src/main/java/com/Timecapsule/timecapsule/controller/ImagesController.java
@@ -1,9 +1,15 @@
 package com.Timecapsule.timecapsule.controller;
 
 
+import com.Timecapsule.timecapsule.dto.ImagesDto.ImagesDto;
+import com.Timecapsule.timecapsule.services.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImagesController {
 
 
+    private final ImageService imageService;
+
+    @GetMapping
+    public ResponseEntity<List<ImagesDto>> getAllImages(){
+      List<ImagesDto> images = imageService.getAll();
+
+      return ResponseEntity.ok(images);
+    }
 }
