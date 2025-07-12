0c04d9590f67222b3085d2f827a2c8cd61c090a1
Jorge <jorgevivar2005@gmail.com>
2025-07-12 18:11:01 +0200

    Add TimeCapsuleApplication class to initialize Spring Boot application

--- src/main/java/com/Timecapsule/timecapsule/TimeCapsuleApplication.java
+++ src/main/java/com/Timecapsule/timecapsule/TimeCapsuleApplication.java
@@ -1,7 +1,4 @@
 package com.Timecapsule.timecapsule;
-import com.Timecapsule.timecapsule.config.CloudinaryConfig;
-import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,9 +6,9 @@ import java.io.IOException;
 import java.util.Map;
 
 @SpringBootApplication
-public class TimecapsuleApplication {
+public class TimeCapsuleApplication {
 
 	public static void main(String[] args) throws IOException {
-		SpringApplication.run(TimecapsuleApplication.class, args);
+		SpringApplication.run(TimeCapsuleApplication.class, args);
 	}
 }
